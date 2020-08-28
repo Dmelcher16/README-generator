e995681e10165dd68f0a660f42b06e8e8dda07a2
Dak <67485000+Dmelcher16@users.noreply.github.com>
2020-08-27 22:08:25 -0700

    adding markdown

--- index.js
+++ index.js
@@ -1,11 +1,76 @@
-// array of questions for user
-const questions = [
+const inquirer = require('inquirer');
+const fs = require("fs");
+const generateMarkdown = require("./utils/generateMarkdown.js")
 
-];
+// array of questions for user
+// const questions = [
+//     {
+//         type: "input",
+//         message: "What is your GitHub username?",
+//         name: "username",
+//     },
+//     {
+//         type: "input",
+//         message: "What is your email address?",
+//         name: "email",
+//     },
+//     {
+//         type: "input",
+//         message: "What is your project's name?",
+//         name: "projectname",
+//     },
+//     {
+//         type: "input",
+//         message: "Please write a short dscription of your project.",
+//         name: "description",
+//     },
+//     {
+//         type: "list",
+//         message: "What kind of license should your project have?",
+//         name: "license",
+//         choices: ["MIT", "GPL", "Apache", "Public Domain (unlicensed)"]
+//     },
+//     {
+//         type: "input",
+//         message: "What command should be run to install dependencies?",
+//         name: "install",
+//     },
+//     {
+//         type: "input",
+//         message: "What command should be run to run tests?",
+//         name: "test",
+//     },
+//     {
+//         type: "input",
+//         message: "What does the user need to know about using the repo?",
+//         name: "info",
+//     },
+//     {
+//         type: "input",
+//         message: "What does the user need to know about contributing to this repo?",
+//         name: "contribute",
+//     }
+// ];
+inquirer.prompt(questions)
+.then(function(response){
+    console.log(response)
+    let docName = response.projectname + "-README.md";
+    let docTitle = "# " + response.projectname;
+    let descriptionTitle = "Description";
+    let description = response.description;
+    // let answers = JSON.stringify(response, null, 2);
+})
 
 // function to write README file
-function writeToFile(fileName, data) {
+function writeToFile(docName, data) {
+        fs.writeFile(docName, docTitle, (err) =>{
+            if (err) throw err;
+            console.log("Checkout your new README!")
+        })
+
 }
+// function writeToFile(projectname.md, data) {
+// }
 
 // function to initialize program
 function init() {
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,30 @@
 // function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
+function generateMarkdown(license, projectname, description, install, info, contribute, test, email, username) {
+  return `
+  license
+  # ${projectname}
+  ## DESCRIPTION
+  ${description}
+  ##TABLE OF CONTENTS
+  [Installation](#installation)
+  [Usage](#usage)
+  [License](#license)
+  [Contributions](#contributions)
+  [Testing](#testing)
+  [Questions](#questions)
+  ## INSTALLATION
+  Please run \`${install}\` in the command line to install dependencies.
+  ## USAGE
+  ${info}
+  ## LICENSE
+  The type of license this project uses is - ${license}
+  ## CONTRIBUTIONS
+  ${contribute}
+  ## TESTING
+  For testing please run \`${test}\` in the command line.
+  ## QUESTIONS
+  All inquiries can be sent to ${email}, and be sure to check out more of my GitHub projects at [${username}](https://www.github.com/${username})
+
 
 `;
 }
